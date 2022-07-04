d5df028d54ef712028c2456d75e4e91b1abe45ab
Vurnex <hamilton.laquon@gmail.com>
2022-07-04 18:28:18 -0500

    Add Mouseover Trail
    Added an event listener that will leave a trail on each cell
    after the mouse hovers over it.

--- js/script.js
+++ js/script.js
@@ -12,9 +12,11 @@ function makeRows(rows, cols) {
 
     let cell = document.createElement("div");
 
-    cell.innerText = (c + 1);
+    //cell.innerText = (c + 1);
 
     container.appendChild(cell).className = "grid-item";
+
+    cell.addEventListener('mouseover', e => e.target.classList.add('grid-item-hover'));
     
   };
 
@@ -22,4 +24,25 @@ function makeRows(rows, cols) {
 
 makeRows(16, 16);
 
+
+function changeBackground() {
+
 let getGrid = document.querySelector(".grid-item");
+
+getGrid.addEventListener("mouseover", function() {
+
+    getGrid.classList.add("grid-item-hover");
+
+});
+
+}
+
+$(document).on("mouseover", ".grid-item", function() {
+
+
+    getGrid.classList.add("grid-item-hover");
+});
+
+$(document).on("mouseleave", ".grid-item", function() {
+    // hover ends code here
+});
